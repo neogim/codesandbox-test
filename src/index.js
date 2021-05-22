7959571163a04a173f4e0c2b71ae029c8b8481de
neogim <75684023+neogim@users.noreply.github.com>
2021-05-22 18:20:01 +0900

    filterについて filterについて

--- src/index.js
+++ src/index.js
@@ -5,14 +5,38 @@
 // console.log(num2);
 // console.log(arr3);
 
-//配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// //配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
+// const arr6 = [...arr4];
 
-arr6[0] = 100;
+// arr6[0] = 100;
 
-console.log(arr6);
-const arr7 = [...arr4, ...arr5];
-console.log(arr7);
+// console.log(arr6);
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "jake"];
+// for (let index =0; index < nameArr.length; index++){
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+nameArr.map((name) => console.log(name));
+
+/**
+ * filterは、returnで返された条件に適合する引数を返す
+ */
+const numArr = [1, 2, 3, 4, 5, 6];
+const newNumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+console.log(newNumArr);
