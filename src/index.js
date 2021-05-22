9166b8f7929ca1eda65e6599916cfbd6f92e3ed4
neogim <75684023+neogim@users.noreply.github.com>
2021-05-22 17:33:16 +0900

    まとめる まとめる

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-<p>修正しました。</p>
-
-`;
+//まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
