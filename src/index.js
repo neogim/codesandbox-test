728ef6c5a6e2b97bd14053d8740dfe314235a597
neogim <75684023+neogim@users.noreply.github.com>
2021-05-22 19:42:20 +0900

    論理演算子の本来の意味 論理演算子の本来の意味

--- src/index.js
+++ src/index.js
@@ -17,55 +17,67 @@
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
 
+// /**
+//  * mapやfilterを使った配列の処理
+//  */
+// const nameArr = ["田中", "山田", "jake"];
+// // for (let index =0; index < nameArr.length; index++){
+// //   console.log(nameArr[index]);
+// // }
+
+// // const nameArr2 = nameArr.map((name)=>{
+// //   return name;
+// // })
+// // console.log(nameArr2);
+
+// nameArr.map((name) => console.log(name));
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "jake") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+// nameArr.map((name) => console.log(name));
+
+// /**
+//  * filterは、returnで書いた条件に一致する引数を返す
+//  */
+// const numArr = [1, 2, 3, 4, 5, 6];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// /**
+//  * 三項演算子
+//  */
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を設定してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です。";
+// };
+
+// console.log(checkSum(50, 110));
 /**
- * mapやfilterを使った配列の処理
+ * 論理演算子の本当の意味
  */
-const nameArr = ["田中", "山田", "jake"];
-// for (let index =0; index < nameArr.length; index++){
-//   console.log(nameArr[index]);
-// }
+// ||は左がfalseなら右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
 
-// const nameArr2 = nameArr.map((name)=>{
-//   return name;
-// })
-// console.log(nameArr2);
-
-nameArr.map((name) => console.log(name));
-
-nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
-
-const newNameArr = nameArr.map((name) => {
-  if (name === "jake") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
-nameArr.map((name) => console.log(name));
-
-/**
- * filterは、returnで書いた条件に一致する引数を返す
- */
-const numArr = [1, 2, 3, 4, 5, 6];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
-
-/**
- * 三項演算子
- */
-
-const num = 1300;
-console.log(num.toLocaleString());
-
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を設定してください";
-console.log(formattedNum);
-
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "100を超えています！！" : "許容範囲内です。";
-};
-
-console.log(checkSum(50, 110));
+//&&は左側がtrueなら右側を返す
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
