76eab72235450ac0c6eea290562eaaa3873c5b8c
neogim <75684023+neogim@users.noreply.github.com>
2021-05-22 17:41:30 +0900

    配列のコピー、結合 配列のコピー、結合

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-//まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// //まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+
+arr6[0] = 100;
+
+console.log(arr6);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
